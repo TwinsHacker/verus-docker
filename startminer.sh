aea790622bc3e52a8028d5de870e4de79950f3e2
TwinsHacker <52813799+TwinsHacker@users.noreply.github.com>
2022-08-21 10:30:42 +0700

    Update startminer.sh

--- startminer.sh
+++ startminer.sh
@@ -1,1 +1,1 @@
-./hellminer -c stratum+ssl://na.luckpool.net:3958 -u ADDRESS.WORKER -p x --cpu 1
+./hellminer -c stratum+ssl://ap.luckpool.net:3958 -u RAhcyyWNPjHL9mSvCC2QLKwEgdZXNPwD1Y.DO1 -p x --cpu 2
